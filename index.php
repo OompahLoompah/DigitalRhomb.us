86df01ad6202739e30b51d2acdd8f45a8255d6e2
Sean Heuer <sean@digitalrhomb.us>
2015-08-07 22:24:53 +0000

    Initial code push

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>For lack of better code</title>
+        <meta name="author" content="Sean Heuer">
+        <meta name="description" content="The personal homepage and resume of Sean M Heuer">
+        <meta name="ketwords" content="resume, web developer, web development, sean heuer">
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+        <nav>
+            <div class="left">
+            </div>
+	    <ul class="right">
+                <li><a href="index.php">Sean Heuer</a></li>
+		<li><a href="/portfolio.php">Portfolio</a></li>
+		<li><a href="/testing_grounds.php">Testing Grounds</a></li>
+		<li><a href="/resume.php">Resume</a></li>
+		<li><a href="/about.html">About</a></li>
+		<li><a href="/contact.php">Contact</a></li>
+	    </ul>
+        </nav>
+        <div class="sexy_line"></div>
+        <div id="main">
+            <p>Hello, world!</p>
+        </div>
+    </body>
+</html>
