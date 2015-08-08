92562be0adfe3eae4cdcba43442aad8457c8e2a7
Sean Heuer <sean@digitalrhomb.us>
2015-08-08 00:16:16 +0000

    Finished non-mobile navbar

---
+++ assets/php/test.php
@@ -0,0 +1,19 @@
+<?php
+
+function get_navbar(){
+    echo '        <nav>
+            <a href="digitalrhomb.us" class="photo">
+                <img src="/assets/images/seanheuer.jpg">
+            </a>
+            <ul>
+                <li><a href="index.php">Sean Heuer</a></li>
+                <li><a href="/contact.php">Contact</a></li>
+                <li><a href="/about.html">About</a></li>
+                <li><a href="/testing_grounds.php">Testing Grounds</a></li>
+                <li><a href="/portfolio.php">Portfolio</a></li>
+            </ul>
+        </nav>
+        <div class="sexy_line"></div>';
+}
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+
+include(dirname(__FILE__)."/assets/php/test.php"); 
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,22 +11,10 @@
         <meta name="author" content="Sean Heuer">
         <meta name="description" content="The personal homepage and resume of Sean M Heuer">
         <meta name="ketwords" content="resume, web developer, web development, sean heuer">
-        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/assets/stylesheets/style.css">
     </head>
     <body>
-        <nav>
-            <div class="left">
-            </div>
-	    <ul class="right">
-                <li><a href="index.php">Sean Heuer</a></li>
-		<li><a href="/portfolio.php">Portfolio</a></li>
-		<li><a href="/testing_grounds.php">Testing Grounds</a></li>
-		<li><a href="/resume.php">Resume</a></li>
-		<li><a href="/about.html">About</a></li>
-		<li><a href="/contact.php">Contact</a></li>
-	    </ul>
-        </nav>
-        <div class="sexy_line"></div>
+        <?php get_navbar(); ?>
         <div id="main">
             <p>Hello, world!</p>
         </div>
