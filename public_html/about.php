5ce24a94269d19859323bde66bab0f58a29d53b4
Sean Heuer <sean@digitalrhomb.us>
2015-08-18 16:04:11 -0400

    Added content to about and contact.

--- public_html/about.php
+++ public_html/about.php
@@ -8,10 +8,14 @@ include(dirname(__FILE__)."/assets/php/common_html_getters.php");
 <html>
     <?php get_header(); ?>
     <body>
+        <div id="wrapper">
         <?php get_navbar(); ?>
-        <div id="main">
-            <p>About me!</p>
+        <div id="body-wrapper">
+            <div class="text-section">
+            <p>Hi! Thanks for checking out my site! My name is Sean Heuer and I'm a through and through geek. I've done work as a Systems Administrator, Programmer, Freelancer, and Groundskeeper so pretty much anything you need I can do! I graduated from the University of Oregon in December 2013 with a B.Sc in Computer and Information Science. From that experience I found that the one thing I love above all else is learning new things so I'm always jumping from project to project. I've written projects from websites to artificial intelligence and always love finding out something new!</p>
+            </div>
         </div>
         <?php get_footer(); ?>
+        </div>
     </body>
 </html>
--- public_html/contact.php
+++ public_html/contact.php
@@ -8,10 +8,14 @@ include(dirname(__FILE__)."/assets/php/common_html_getters.php");
 <html>
     <?php get_header(); ?>
     <body>
+        <div id="wrapper">
         <?php get_navbar(); ?>
-        <div id="main">
-            <p>Contact me!</p>
+        <div id="body-wrapper">
+            <div class="text-section">
+                <p>eMail: sean@digitalrhomb.us <br>Google Hangouts: sean.m.g.heuer@gmail.com</p>
+            </div>
         </div>
     <?php get_footer(); ?>
+    </div>
     </body>
 </html>
